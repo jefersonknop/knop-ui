895c7199809126ff6eb2d6b51ab726a1d0884278
jefersonknop <jefersonknop@gmail.com>
2019-07-04 13:13:59 -0300

    update04072019

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const app = express();
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
 app.use(express.static(__dirname + '/dist/KNOP-UI'));
 
-app.get('/*', function(req,res) {
-    
-//res.sendFile(path.join(__dirname+'/dist/KNOP-UI/index.html'));
-res.sendFile(path.join(__dirname+'/dist/KNOP-UI/'));
+app.get('/*', function(req,res) {    
+
+   res.sendFile(path.join(__dirname+'/dist/KNOP-UI/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200, ()=>{
+    console.log('servidor executado');
+  });
